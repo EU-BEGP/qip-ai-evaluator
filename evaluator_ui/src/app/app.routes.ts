6e8f4f6b5ae2612dff4856aa10609d9907286966
SebastianItamari <sebaton66@gmail.com>
2025-11-03 19:17:18 -0400

    Create the main pages in UI

--- evaluator_ui/src/app/app.routes.ts
+++ evaluator_ui/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login';
+import { EvaluationComponent } from './pages/evaluation/evaluation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'evaluation', component: EvaluationComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
