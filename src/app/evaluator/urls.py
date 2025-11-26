a7e3b5f1a3b6cc08af6989c751c8ccf9d46bbb0e
Santiago Almancy <santialmancy@gmail.com>
2025-11-26 00:20:43 -0400

    Update urls for extract metadata

--- src/app/evaluator/urls.py
+++ src/app/evaluator/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import evaluate_module, module_last_modified
+from .views import evaluate_module, module_last_modified, get_module_metadata
 
 urlpatterns = [
     # POST /api/evaluate/
     path('evaluate/', evaluate_module, name='evaluate_module'),
     path('module_last_modified/', module_last_modified, name='module_last_modified'),
+    path('extract_metadata/', get_module_metadata, name='extract_metadata'),
 ]
