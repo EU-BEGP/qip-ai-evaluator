d667107db9b10f5a3d2416f277c307dac2881e34
Santiago Almancy <santialmancy@gmail.com>
2025-12-22 19:51:11 -0400

    Add new configurations and improve deployment for development and production for evaluator api

--- evaluator_api/evaluator_api/urls.py
+++ evaluator_api/evaluator_api/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/users/', include('apps.users.urls')),
+    path('api/evaluations/', include('apps.evaluations.urls')),
+    path('api/notifications/', include('apps.notifications.urls')),
 ]
