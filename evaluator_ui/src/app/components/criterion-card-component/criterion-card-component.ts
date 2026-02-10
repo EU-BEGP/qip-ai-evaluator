e5e7eaf631de780a2770f7b5488afca50a02ee4a
FredAntB <antoniobalderramamendoza@gmail.com>
2026-02-09 23:55:39 -0400

    add criterion card component and the self-assesment page with the connection to the backend via the self-assessment service

---
+++ evaluator_ui/src/app/components/criterion-card-component/criterion-card-component.ts
@@ -0,0 +1,36 @@
+// Copyright (c) Universidad Privada Boliviana (UPB) - EU-BEGP
+// MIT License - See LICENSE file in the root directory
+// Sebastian Itamari, Santiago Almancy, Alex Villazon
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-criterion-card-component',
+  imports: [MatCardModule, CommonModule, MatButtonModule],
+  templateUrl: './criterion-card-component.html',
+  styleUrls: ['./criterion-card-component.css'],
+})
+export class CriterionCardComponent implements OnInit {
+  @Input() question: string = '';
+  @Input() description: string = '';
+  @Input() buttons: Array<{ label: string; value?: any; color?: string }> = [];
+
+  @Output() buttonClick = new EventEmitter<{ value: any }>();
+  @Output() aiClick = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onButtonClick(btn: { label: string; value?: any }, event: MouseEvent) {
+    event.stopPropagation();
+    this.buttonClick.emit({ value: btn.value ?? btn.label });
+  }
+
+  onAiClick(event: MouseEvent) {
+    event.stopPropagation();
+    this.aiClick.emit();
+  }
+}
