f23b37575109fc1b404fd477c26f0b1841a49ad7
FredAntB <antoniobalderramamendoza@gmail.com>
2026-02-11 14:31:53 -0400

    fixed button style in criterion card for selection clarity

--- evaluator_ui/src/app/components/criterion-card-component/criterion-card-component.ts
+++ evaluator_ui/src/app/components/criterion-card-component/criterion-card-component.ts
@@ -5,17 +5,31 @@ import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-criterion-card-component',
   imports: [MatCardModule, CommonModule, MatButtonModule],
   templateUrl: './criterion-card-component.html',
   styleUrls: ['./criterion-card-component.css'],
+  animations: [
+    trigger('clickAnimation', [
+      transition('* => *', [
+        style({ transform: 'scale(0.95)' }),
+        animate('100ms ease-out', style({ transform: 'scale(1)' })),
+      ]),
+    ]),
+  ],
 })
 export class CriterionCardComponent implements OnInit {
   @Input() question: string = '';
   @Input() description: string = '';
-  @Input() buttons: Array<{ label: string; value?: any; color?: string }> = [];
+  @Input() buttons: Array<{
+    label: string;
+    value?: any;
+    color?: string;
+    state?: boolean;
+  }> = [];
 
   @Output() buttonClick = new EventEmitter<{ value: any }>();
   @Output() aiClick = new EventEmitter<void>();
@@ -24,8 +38,15 @@ export class CriterionCardComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onButtonClick(btn: { label: string; value?: any }, event: MouseEvent) {
+  onButtonClick(
+    btn: { label: string; value?: any; state: boolean },
+    event: MouseEvent,
+  ) {
     event.stopPropagation();
+    if (!btn.state) {
+      this.buttons.forEach((b) => (b.state = false));
+      btn.state = !btn.state;
+    }
     this.buttonClick.emit({ value: btn.value ?? btn.label });
   }
 
