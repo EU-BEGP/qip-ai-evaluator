40f58976b260ac67a08f80c5ca874c6f532fe09d
Santiago Almancy <santialmancy@gmail.com>
2026-04-29 18:13:16 -0400

    (BACKEND): Delete reference to unused old wrappers.

--- src/app/rag/model_wrapper/__init__.py
+++ src/app/rag/model_wrapper/__init__.py
@@ -6,9 +6,7 @@ import logging
 from typing import Dict
 
 from .base import BaseLLMWrapper
-from .models.gemini_wrapper import GeminiWrapper
 from .models.groq_wrapper import GroqWrapper
-from .models.ollama_wrapper import OllamaWrapper
 
 logger = logging.getLogger(__name__)
 
@@ -18,15 +16,7 @@ def get_llm_wrapper(cfg: Dict) -> BaseLLMWrapper:
     llm_cfg = cfg.get("llm_settings", {})
     wrapper_name = llm_cfg.get("wrapper")
 
-    if wrapper_name == "ollama":
-        logger.info("Loading Ollama wrapper")
-        return OllamaWrapper(cfg)
-
-    elif wrapper_name == "gemini":
-        logger.info("Loading Gemini wrapper")
-        return GeminiWrapper(cfg)
-
-    elif wrapper_name == "groq":
+    if wrapper_name == "groq":
         logger.info("Loading Groq wrapper")
         return GroqWrapper(cfg)
 
